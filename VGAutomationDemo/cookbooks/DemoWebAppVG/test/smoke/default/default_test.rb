bf0eed795942c2b5bfcb77449cfa8478b99ffa31
ulkarsh <ulkarshdev@gmail.com>
2017-12-06 03:01:37 +0530

    slight modification in unit tests

--- VGAutomationDemo/cookbooks/DemoWebAppVG/test/smoke/default/default_test.rb
+++ VGAutomationDemo/cookbooks/DemoWebAppVG/test/smoke/default/default_test.rb
@@ -2,17 +2,30 @@
 
 # Inspec test for recipe DemoWebApp::default
 
-describe package('httpd') do
-  it { should be_installed }
-end
-
 describe package('unzip') do
 	it { should be_installed }
 end
 
-describe service('httpd') do
-  it { should be_enabled }
-  it { should be_running }
+control 'httpd' do
+  impact 0.7
+  title 'Verify httpd service'
+  desc 'Ensures httpd service is up and running'
+	describe package('httpd') do
+		  it { should be_installed }
+		end
+	describe service('httpd') do  
+	  it { should be_enabled }
+	  it { should be_running }
+	end
+end
+
+control 'file' do
+  impact 0.7
+  title 'Verify index file'
+  desc 'Ensures index file is created'
+	describe file('/var/www/html/index.html') do  
+	  it { should exist }	  
+	end
 end
 
 describe command('curl localhost') do
@@ -23,9 +36,18 @@ describe port(80) do
   it { should be_listening }
 end
 
-describe service('chef-client') do
-  it { should be_enabled }
-  it { should be_running }
+describe user('web_admin') do
+  it { should exist }
+end
+
+control 'chef-client' do
+  impact 0.7
+  title 'Verify chef-client service'
+  desc 'Ensures chef-client service is up and running'
+	describe service('chef-client') do  
+	  it { should be_enabled }
+	  it { should be_running }
+	end
 end
 
 describe package('vim-enhanced') do
