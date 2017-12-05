ba27a1608658558172b32df854af8956663a69dd
ulkarsh <ulkarshdev@gmail.com>
2017-12-05 21:28:29 +0530

    modified for kitchen testing. Added template file

--- AWSAutomationDemo/cookbooks/DemoWebApp/recipes/default.rb
+++ AWSAutomationDemo/cookbooks/DemoWebApp/recipes/default.rb
@@ -1,5 +1,20 @@
-#
-# Cookbook:: DemoWebApp
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  mode '0644'
+  owner 'web_admin'
+  group 'web_admin'
+end
