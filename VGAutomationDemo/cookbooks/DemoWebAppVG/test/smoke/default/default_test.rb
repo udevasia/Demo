c7b9e5ee6763720e7e5f64da366f1d4c0468dcc4
ulkarsh <ulkarshdev@gmail.com>
2017-12-06 01:39:09 +0530

    added sample tests

--- VGAutomationDemo/cookbooks/DemoWebAppVG/test/smoke/default/default_test.rb
+++ VGAutomationDemo/cookbooks/DemoWebAppVG/test/smoke/default/default_test.rb
@@ -2,17 +2,19 @@
 
 # Inspec test for recipe DemoWebApp::default
 
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package('httpd') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service('httpd') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command('curl localhost') do
+  its('stdout') { should match /My FQDN/ }
+end
+
+describe port(80) do
+  it { should be_listening }
 end
